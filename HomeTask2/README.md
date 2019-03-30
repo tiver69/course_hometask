5967b367281fb8a524a156c5df129355e9244826
Aleksandra <kibko.sasha@gmial.com>
2019-03-31 00:05:42 +0200

    Add src and lib files before compile for Home Task2

---
+++ HomeTask2/README.md
@@ -0,0 +1,1 @@
+Course Hometask 2
---
+++ HomeTask2/src/main/java/hometask/console/Main.java
@@ -0,0 +1,11 @@
+package hometask.console;
+
+import org.junit.Assert;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("My Name is Aleks Kibko");
+        Assert.fail("Are you sure?");
+    }
+}
